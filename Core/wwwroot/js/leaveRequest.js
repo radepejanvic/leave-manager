6ba1cd60d67c8db89a00c2540aeac9ed5c2b61cd
Rade Pejanovic <radepejanovic@gmail.com>
2024-08-13 16:22:13 +0200

    feat: Add reason and type to table display

--- Core/wwwroot/js/leaveRequest.js
+++ Core/wwwroot/js/leaveRequest.js
@@ -1,44 +1,74 @@
-﻿var dataTable;
+﻿function format(d) {
+    // `d` is the original data object for the row
+    return (
+        '<dl>' +
+        '<dt>Leave request content</dt>' +
+        '<dd>' +
+        d.reason +
+        '</dd>' +
+        '</dl>'
+    );
+}
 
-$(document).ready(function () {
-    loadDataTable();
-});
-
-function loadDataTable() {
-    dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/leaveRequest/getall' },
-        "columns": [
-            { data: 'employeeEmail', "width": "20%"},
-            {
-                data: 'start',
-                "render": function (data) {
-                    let date = new Date(data);
-                    return date.toDateString();
-                },
-                "width": "20%"
+let table = new DataTable('#tblData', {
+    ajax: '/admin/leaveRequest/getall',
+    columns: [
+        {
+            className: 'dt-control',
+            orderable: false,
+            data: null,
+            defaultContent: ''
+        },
+        { data: 'employeeEmail', "width": "20%" },
+        {
+            data: 'start',
+            "render": function (data) {
+                let date = new Date(data);
+                return date.toDateString();
             },
-            {
-                data: 'end',
-                "render": function (data) {
-                    let date = new Date(data);
-                    return date.toDateString();
-                },
-                "width": "20%"
+            "width": "15%"
+        },
+        {
+            data: 'end',
+            "render": function (data) {
+                let date = new Date(data);
+                return date.toDateString();
             },
-            { data: 'duration', "width": "20%" },
-            {
-                data: 'id',
-                "render": function (data) {
-                    return `<div class="w-75 btn-group" role="group">
+            "width": "15%"
+        },
+        {
+            data: 'duration',
+            "className": "text-end"
+        },
+        {
+            data: 'type',
+            "className": "text-end"
+        },
+        {
+            data: 'id',
+            "render": function (data) {
+                return `<div class="btn-group w-100" role="group">
                      <a href="/admin/leaveRequest/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
                      <a onClick=Delete('/admin/leaveRequest/delete?id=${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
-                },
-                "width": "25%"
-            }
-        ]
-    });
-}
+            },
+            "width": "25%"
+        }
+    ],
+    order: [[1, 'asc']]
+});
+
+table.on('click', 'td.dt-control', function (e) {
+    let tr = e.target.closest('tr');
+    let row = table.row(tr);
+
+    if (row.child.isShown()) {
+        row.child.hide();
+    }
+    else {
+        row.child(format(row.data())).show();
+    }
+});
 
 function Delete(url) {
     Swal.fire({
